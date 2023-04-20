8893db1534b34c4a6bb2cef472f044f7401a9c8a
RaviPrasad7 <22418051-RaviPrasad7@users.noreply.replit.com>
2023-04-20 09:45:58 +0000

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+import HomeContainer from './containers/HomeContainer'
+
+export default function App() {
+  return (
+    <main>
+      React ⚛️ + Vite ⚡ + Replit 🌀
+      <HomeContainer />
+    </main>
+  )
+}
---
+++ src/containers/HomeContainer.tsx
@@ -0,0 +1,5 @@
+const HomeContainer: React.FC<any> = (): JSX.Element => {
+  return <>This is home container</>;
+};
+
+export default HomeContainer;
